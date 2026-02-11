1ef9271a7f09787544a05b5e6397e885546cfce1
Atul Kumar Suthar <99497472+SutharAtul@users.noreply.github.com>
2026-02-11 17:04:43 +0530

    Fix nested repo and clean project structure

---
+++ server/routes/contactRoutes.js
@@ -0,0 +1,35 @@
+import express from "express";
+import Contact from "../models/Contact.js";
+
+const router = express.Router();
+
+// Save Contact Form
+router.post("/", async (req, res) => {
+  try {
+    const { name, email, message } = req.body;
+
+    if (!name || !email || !message) {
+      return res.status(400).json({ error: "All fields required" });
+    }
+
+    const newContact = new Contact({
+      name,
+      email,
+      message
+    });
+
+    await newContact.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Message sent successfully"
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "Server error"
+    });
+  }
+});
+
+export default router;
